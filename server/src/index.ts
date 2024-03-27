c73d80f901bfb0a9ab16e29ee362a32f480b9d52
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-03-27 11:14:00 +0800

    delete unnecessary files like socket.io

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,8 +6,6 @@ import router from "./router";
 import cors from "cors";
 import mongoose from "mongoose";
 // import socketIO from "socket.io"; // Import the socket.io module
-import { Server as SocketIOServer, Socket } from "socket.io";
-import socketController from './controller/RealTime/SocketController'
 
 
 
@@ -18,15 +16,6 @@ const app = express();
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
-
-const io = new SocketIOServer(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT"], // Allow GET, POST, and PUT requests
-  },
-});
-
-socketController(io);
 
 server.listen(port, () => {
   console.log("Server running on http://localhost:" + port);
