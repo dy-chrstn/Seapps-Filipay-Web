2862a2e11d19e233b21d5d919e1ee645a34962a0
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-04-02 15:50:23 +0800

    comit for pull

--- client/src/pages/ClientSubPages/TransportCooperative.tsx
+++ client/src/pages/ClientSubPages/TransportCooperative.tsx
@@ -13,8 +13,8 @@ const TransportCooperative = () => {
 
   return (
     <div className='w-screen h-screen transparent-caret'>
-      <Header title="Dashboard" onClick={openMenu}/> 
-      {menuOpen ? <Menu/> : <MiniMenu/>}
+      <Header title="Transport Cooperative" onClick={openMenu}/> 
+       {menuOpen ? <Menu title={"Vehicle"}/> : <MiniMenu title={"Vehicle"}/> }
 
       <div className='flex flex-col flex-grow'>
         {/* Always render TransportCoopTable regardless of menu state */}
