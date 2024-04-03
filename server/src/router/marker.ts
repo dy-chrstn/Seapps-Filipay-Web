4bfb9f12c8e0ac7a271c556ebd63a7c6f88f143a
Andrei <dy.cavd@gmail.com>
2024-04-03 17:11:26 +0800

    Get markers logic updated

--- server/src/router/marker.ts
+++ server/src/router/marker.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 import { getMarkers, registerMarker, updateMarkerById, deleteMarker } from '../controller/marker';
 export default (router: express.Router) => {
-    router.get('/getMarkers', getMarkers);
+    router.get('/getMarkers/:id', getMarkers);
     router.post('/registerMarker', registerMarker);
     router.delete('/deleteMarker/:id', deleteMarker);
     router.patch('/updateMarkerById/:id', updateMarkerById);
