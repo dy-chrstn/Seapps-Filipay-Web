5a7565ae5d9d4673587bbe09f727147485085e59
Christian Andrei Dy <dy.cavd@gmail.com>
2024-04-24 12:51:54 +0800

    Contant file added

---
+++ client/src/contants/contants.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = import.meta.env.VITE_DOMAIN
+export const USERNAME = import.meta.env.VITE_USERNAMEE
+export const PASSWORD = import.meta.env.VITE_PASSWORD
+export const GOOGLE_API = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
