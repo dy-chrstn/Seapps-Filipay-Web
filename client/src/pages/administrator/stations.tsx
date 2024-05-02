f129c08add2165bb3415073e6831b74abc4f97f1
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-05-02 11:51:36 +0800

    addded header, and menu in stations page

--- client/src/pages/administrator/stations.tsx
+++ client/src/pages/administrator/stations.tsx
@@ -1,12 +1,40 @@
-import MapContainer from "../../components/Map";
+import { useState } from 'react'
+import Header from '../../components/admin/header';
+import Menu from '../../components/admin/Menu/menu';
+import MiniMenu from '../../components/admin/Menu/miniMenu';
+import MapContainer from '../../components/Map';
 
-const Map = () => {
+const Map: React.FC = () => {
 
-    return (
-        <div>
-            <MapContainer />
+    const [menuOpen, setMenuOpen] = useState(false)
+    const openMenu = () => {
+      if(!menuOpen){
+        setMenuOpen(true)
+      } else
+      setMenuOpen(false)
+    }
+
+    
+return (
+    <div className='w-screen h-screen'>
+        <Header title="Stations" onClick = {openMenu}/> 
+        <div className='flex flex-row'>
+            <div className={`${menuOpen ?'w-[20%]' : 'w-[5%]'} `}>
+              {menuOpen ? <Menu title={"Stations"}/> : <MiniMenu title={"Stations"}/> }
+            </div>
+          
+            <div className='h-full w-[100%] mx-10 mt-5 mb-10 z-[-10]'>
+              <MapContainer/>
+            </div>
+             
+          
         </div>
-    );
+
+    </div>
+  );
+
 }
 
-export default Map
+
+export default Map;
+
